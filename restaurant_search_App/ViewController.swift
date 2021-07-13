6859d45f4313e30cd1dbfa0c064b5b0e0c8ec2be
junshu310 <shunki1107@gmail.com>
2021-07-13 18:10:00 +0900

    Initial Commit

---
+++ restaurant_search_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  restaurant_search_App
+//
+//  Created by 佐藤駿樹 on 2021/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
